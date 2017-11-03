315074f5a65f0ca395ffdb72e880666080e37467
David Weis <dweis7@gmail.com>
2017-11-03 10:58:44 +0100

    Add Person objects

--- SWA Assignment 2/Controllers/HomeController.cs
+++ SWA Assignment 2/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SWA_Assignment_2.Users;
 
 namespace SWA_Assignment_2.Controllers
 {
@@ -20,7 +21,7 @@ namespace SWA_Assignment_2.Controllers
 
         public ActionResult GetJson()
         {
-            return Json(new { test = "Lol" });
+            return Json(new Person{Name = new Name{ FirstName = "Nico", LastName = "Sandival"}});
         }
     }
 }
---
+++ SWA Assignment 2/Users/Name.cs
@@ -0,0 +1,8 @@
+﻿namespace SWA_Assignment_2.Users
+{
+    public class Name
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+}
+}
